f5a127f32af989239d4b8bf43121b635fbc2671d
yoichi-aoyama <aoyama.yoichi@gmail.com>
2023-02-26 23:33:21 +0900

    open index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
+const manifest = defineManifest({
+    manifest_version:3,
+    name: "chrome ext sample",
+    version: "0.0.1",
+    permissions: ["bookmarks"],
+    action: {
+        default_popup: "index.html",
+    },
+});
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
 })
